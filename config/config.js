659cf2d346faae9892413bcbe76da34326fed4df
Mark Henke <gilligan128@gmail.com>
2016-02-29 23:01:32 -0500

    fixed start param

--- config/config.js
+++ config/config.js
@@ -4,19 +4,4 @@ module.exports ={
   "username"              :     "root",
   "password"              :     "",
   "database"              :     "DB NAME",
-  facebookAuth: {
-   clientID: 'YOUR-FB-CLIENT-ID',
-   clientSecret: 'YOUR-FB-CLIENT-SECRET',
-   callbackURL: 'http://localhost:3000/auth/facebook/callback',
- },
-  twitterAuth: {
-   consumerKey: 'YOUR-TWITTER-CONSUMER-KEY',
-   consumerSecret: 'YOUR-TWITTER-CONSUMER-SECRET',
-   callbackURL: 'http://localhost:3000/auth/twitter/callback',
- },
- googleAuth: {
-   clientID: 'YOUR-GOOGLE-CLIENT-ID',
-   clientSecret: 'YOUR-GOOGLE-CLIENT-SECRET',
-   callbackURL: 'http://localhost:3000/auth/google/callback',
- },
 };

34ae2897ded01abdcf469a29767b0bf46ff34919
Mark Henke <gilligan128@gmail.com>
2016-03-01 10:39:28 -0500

    idk what is oging on with callback uris

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,6 @@ module.exports ={
   facebookAuth: {
    clientID: '108195792906979',
    clientSecret: 'df136f6372b4c25b91b7cece403b2375',
-   callbackURL: 'http://localhost:5000/auth/facebook/callback',
  },
   twitterAuth: {
    consumerKey: 'YOUR-TWITTER-CONSUMER-KEY',
